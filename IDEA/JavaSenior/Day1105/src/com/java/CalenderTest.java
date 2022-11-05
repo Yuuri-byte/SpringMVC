7ab9f95868fbb255ab0ec75a6253e47dbb9bdd9d
Yuuri <Yuuri_byte@163.com>
2022-11-06 02:24:36 +0800

    1

---
+++ IDEA/JavaSenior/Day1105/src/com/java/CalenderTest.java
@@ -0,0 +1,35 @@
+package com.java;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+/*
+抽象类
+1.创建子类对象
+     OR
+2.调用静态方法
+ */
+public class CalenderTest {
+
+    @Test
+    public void test(){
+        Calendar c1 = Calendar.getInstance();
+        System.out.println(c1.getClass());
+
+        //常用方法
+
+        //get()
+        int days = c1.get(Calendar.DAY_OF_MONTH);
+        System.out.println(days);
+
+        //set()
+        c1.set(Calendar.DAY_OF_MONTH,22);
+        int days2 = c1.get(Calendar.DAY_OF_MONTH);
+        System.out.println(days2);
+
+
+        //add()
+
+    }
+}
