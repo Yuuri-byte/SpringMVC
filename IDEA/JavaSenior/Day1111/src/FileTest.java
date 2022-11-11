982ae700b3db5ae69f9e7bf8eb3effb02f6ba6b7
YuuR1 <Yuuri_byte@163.com>
2022-11-11 14:55:48 +0800

    1111

---
+++ IDEA/JavaSenior/Day1111/src/FileTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import java.io.File;
+public class FileTest {
+
+    @Test
+            public void test1(){
+        //创建File类实例
+        File file = new File("hello.txt");//相对当前moudle
+
+    }
+
+
+
+}
