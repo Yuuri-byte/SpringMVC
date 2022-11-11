909efbf8563485e33f15a5c14575c51b1dab1c13
YuuR1 <Yuuri_byte@163.com>
2022-11-11 18:43:32 +0800

    file

--- IDEA/JavaSenior/Day1111/src/FileTest.java
+++ IDEA/JavaSenior/Day1111/src/FileTest.java
@@ -1,12 +1,16 @@
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+
 public class FileTest {
 
     @Test
-            public void test1(){
+    public void test1() throws IOException {
         //创建File类实例
         File file = new File("hello.txt");//相对当前moudle
+        file.createNewFile();//创建文件
+        
 
     }
 
